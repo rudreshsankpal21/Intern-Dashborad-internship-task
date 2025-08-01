14e02d867a625a2f931b3ded0813b5054cabd7d0
rudresh21 <rudreshsankpal21@gmail.com>
2025-08-01 23:02:43 +0530

    fix : fixed index.css error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./Login";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
